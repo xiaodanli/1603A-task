35abfabf5e41f764950b1beac454d5e210fb5bd0
zhc <1260884064@qq.com>
2018-06-07 21:25:04 +0800

    header完成

--- teamOne/src/js/main.js
+++ teamOne/src/js/main.js
@@ -1,3 +1,26 @@
 var mySwiper = new Swiper('.bigSwiper', {
 
 });
+
+var scroll = new BScroll('.one', {
+    click: true,
+    scrollX: true,
+    scrollY: false
+});
+var ul = document.getElementById('ul');
+var lis = ul.querySelectorAll('#ul>li');
+var sum = 0;
+lis.forEach(function(item, i) {
+    sum += item.offsetWidth + 21;
+})
+ul.style.width = sum / 100 + "rem";
+$('.roll li').on('tap', function() {
+    var index = $(this).index();
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    scroll.scrollToElement(this, 500, true, 0);
+    mySwiper.slideTo(index);
+});
+$('.classify').on('tap', function() {
+    alert('亲！这是低配唯品会，咱无分类项。')
+})
