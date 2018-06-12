139b2e8c4b19973cff5c3833d0bd9192560155bf
whf <787146687@qq.com>
2018-06-13 00:23:33 +0800

    修改测试完成

---
+++ team5/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+var babel = require('gulp-babel');
+var preset = require('babel-preset-es2015');
+var data = require('./data/list.json');
+gulp.task('server', function() {
+    gulp.src('src')
+        .pipe(server({
+            port: 9090,
+            open: true,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+
+                if (pathname === '/favicon.ico') {
+                    return false;
+                }
+                if (pathname === '/api/server') {
+                    res.end(JSON.stringify(data))
+                } else {
+                    pathname = pathname === '/' ? '/index.html' : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+                }
+
+            }
+        }))
+})
