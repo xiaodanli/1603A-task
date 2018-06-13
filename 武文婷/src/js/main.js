35176723d30edd07e0bed0e7bb3da1b05daf5cf7
wwt <1258776978@qq.com>
2018-06-13 20:26:55 +0800

    武文婷作业完成

---
+++ 武文婷/src/js/main.js
@@ -0,0 +1,8 @@
+require.config({
+    baseUrl: '/js/',
+    paths: {
+        'jquery': './libs/jquery-1.8.3.min',
+        'handlebars': './libs/handlebars-v4.0.11',
+        'index': './app/index'
+    }
+})
