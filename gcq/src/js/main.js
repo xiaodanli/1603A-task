a3453869c2257bde16e28e826b2cd133864154a9
gcq <2286680249@qq.com>
2018-06-14 00:26:14 +0800

    盖超群

---
+++ gcq/src/js/main.js
@@ -0,0 +1,12 @@
+require.config({
+    baseUrl: '/js/',
+    paths: {
+        //库js
+        'jquery': 'libs/jquery',
+        'handlebars': 'libs/handlebars-v4.0.11',
+
+
+        //个人js
+        'index': 'app/index'
+    }
+})
