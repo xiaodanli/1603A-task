a12b1cbf1f8b53dbb581afdf8165e02aa894dc82
lee <786722735@qq.com>
2018-06-13 23:26:43 +0800

    李小莹

---
+++ 李小莹/src/js/main.js
@@ -0,0 +1,11 @@
+require.config({
+    baseUrl: '/js/',
+    paths: {
+        //库
+        'jquery': 'lib/jquery-1.7.1.min',
+        'handlebars': 'lib/handlebars-v4.0.11',
+
+        //app
+        'index': 'app/index'
+    }
+})
