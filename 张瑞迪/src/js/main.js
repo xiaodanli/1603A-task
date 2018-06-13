394b24f8727cc9ccf38b0bc3bd2df356e86afffa
zrd <2632730354@qq.com>
2018-06-13 21:10:58 +0800

    作业完成

---
+++ 张瑞迪/src/js/main.js
@@ -0,0 +1,9 @@
+require.config({
+    baseUrl: './js/',
+    paths: {
+        "index": "app/index",
+        "jquery": "libs/jquery",
+        "handlebars": "libs/handlebars-v4.0.11",
+        "render": "common/render"
+    }
+})
