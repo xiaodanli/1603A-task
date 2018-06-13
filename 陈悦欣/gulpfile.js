437085b6a3ff0e8c8bcfc720dc66fb213d4b678e
yuexin <2206373369@qq.com>
2018-06-13 23:54:27 +0800

    陈悦欣

---
+++ 陈悦欣/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var list = require('./data/list.json');
+
+gulp.task('server', function() {
+    gulp.src('src')
+        .pipe(server({
+            port: 8888,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    return false;
+                }
+                if (pathname === '/api/list') {
+                    res.end(JSON.stringify(list));
+                } else {
+                    pathname = pathname === '/' ? '/index.html' : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+                }
+            }
+        }))
+})
