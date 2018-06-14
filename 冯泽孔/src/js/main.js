bec9fc801d1d09f32e5086c3581b2c41e32ba7b8
con <418644241@qq.com>
2018-06-14 09:48:25 +0800

    完成

---
+++ 冯泽孔/src/js/main.js
@@ -0,0 +1,11 @@
+require.config({
+    baseUrl: '/js/',
+    paths: {
+        //libs
+        'jquery': './libs/jquery.min',
+        'handlebars': './libs/handlebars-v4.0.11',
+        //app
+        'index': './app/index',
+        'render': './app/render'
+    }
+})
